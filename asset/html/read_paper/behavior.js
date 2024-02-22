2bc52cdc2224397171fdd0078686e8890823d925
zzls-cool@company <zzls-cool@foxmail.com>
2024-02-22 17:32:38 +0800

    错误修正

--- asset/html/read_paper/behavior.js
+++ asset/html/read_paper/behavior.js
@@ -1,15 +1,1 @@
 window.onscroll = function() {scrollFunction()};
- 
-function scrollFunction() {console.log(121);
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementsByClassName("back-top").style.display = "block";
-    } else {
-        document.getElementsByClassName("back-top").style.display = "none";
-    }
-}
- 
-// 点击按钮，返回顶部
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
