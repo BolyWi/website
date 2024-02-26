ac3b2cf1bc978721f77359fea6e60d39ff3685b3
zzls-cool@company <zzls-cool@foxmail.com>
2024-02-26 15:12:24 +0800

    添加目录

--- asset/html/read_paper/behavior.js
+++ asset/html/read_paper/behavior.js
@@ -1,15 +1,34 @@
-window.onscroll = function() {scrollFunction()};
- 
-function scrollFunction() {console.log(121);
+window.onscroll = function () {
+    scrollFunction();
+};
+
+window.onload = function(){
+    genterateContentList();
+}
+
+function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
 }
- 
+
 // 点击按钮，返回顶部
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function genterateContentList() {
+    let pTitle = document.getElementsByClassName("paragraph-title");
+    let contentTitle = document.getElementById("content-list");
+    for (var i = 0; i < pTitle.length; i++){
+        let link = document.createElement('a')
+        let list = document.createElement('li');
+        list.id = "list-content" + i;
+        link.innerText = pTitle[i].innerText;
+        list.appendChild(link);
+        contentTitle.appendChild(list);
+    }
 }
