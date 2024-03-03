157b2d5828d9d034ad74c65d6e411c3b6a3997f7
走在路上cool <zzls-cool@foxmail.com>
2024-03-03 22:46:28 +0800

    首页日期样式修改

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,6 +1,13 @@
 setInterval(function () { getLocalTime(); }, 1000);
 
 function getLocalTime() {
-    let nowTime = new Date().toLocaleString()
-    document.getElementById('datetime').innerText = nowTime;
+    let nowTime = new Date();
+    let year = nowTime.getFullYear();
+    let month = nowTime.getMonth();
+    let date = nowTime.getDate();
+    let description = "现在是北京时间：";
+    let dateStr = year + "年" + month + "月" + date + "日";
+    let timeStr = nowTime.toLocaleTimeString();
+    let dateTime = document.getElementById('datetime');
+    dateTime.innerText = description + dateStr + " " + timeStr;
 }
