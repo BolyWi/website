c603a34319e576e55204193d3c35b04fda10ae16
zzls-cool@company <zzls-cool@foxmail.com>
2024-03-04 13:14:10 +0800

    首页样式修改，添加按钮

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,13 +1,32 @@
-setInterval(function () { getLocalTime(); }, 1000);
+let dateTime = document.getElementById('datetime');
+let description = dateTime.innerText;
 
-function getLocalTime() {
+var nowTime = function getLocalTime() {
     let nowTime = new Date();
     let year = nowTime.getFullYear();
     let month = nowTime.getMonth();
     let date = nowTime.getDate();
-    let description = "现在是北京时间：";
     let dateStr = year + "年" + month + "月" + date + "日";
     let timeStr = nowTime.toLocaleTimeString();
-    let dateTime = document.getElementById('datetime');
     dateTime.innerText = description + dateStr + " " + timeStr;
+}
+
+setInterval(nowTime, 1000);
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top-button").style.display = "block";
+    } else {
+        document.getElementById("top-button").style.display = "none";
+    }
+}
+
+// 点击按钮，返回顶部
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
