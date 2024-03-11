650c35e750c9be6293d09662220a8db06174f822
走在路上cool <zzls-cool@foxmail.com>
2024-03-11 21:21:31 +0800

    时间显示函数更新

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,14 +1,16 @@
+const weekArray=["日","一","二","三","四","五","六"];
 let dateTime = document.getElementById('datetime');
 let description = dateTime.innerText;
 
-var nowTime = function getLocalTime() {
+let nowTime = function getLocalTime() {
     let nowTime = new Date();
     let year = nowTime.getFullYear();
-    let month = nowTime.getMonth();
+    let month = nowTime.getMonth() + 1;
     let date = nowTime.getDate();
     let dateStr = year + "年" + month + "月" + date + "日";
+    let weekStr = "星期"+ weekArray[nowTime.getDay()];
     let timeStr = nowTime.toLocaleTimeString();
-    dateTime.innerText = description + dateStr + " " + timeStr;
+    dateTime.innerText = description + dateStr + " " + weekStr + " " + timeStr;
 }
 
 setInterval(nowTime, 1000);
